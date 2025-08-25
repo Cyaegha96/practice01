68e5d19cd729fc10b421a9da01a5b83e7fcac9d4
Carrythefuture <146246027+Carrythefuture@users.noreply.github.com>
2025-08-25 16:39:29 +0900

    MembersDao Update

--- src/main/java/dao/MembersDAO.java
+++ src/main/java/dao/MembersDAO.java
@@ -24,7 +24,7 @@ public class MembersDAO {
 		}	
 		return instance;
 	}
-
+	//주석 추가
 	public Connection getConnection() throws Exception{
 		Context ctx = new InitialContext();
 		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
